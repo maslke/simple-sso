eb6f6759cb48010a4ab24d6eb2087289349d2725
maslke <maslke@outlook.com>
2018-09-11 20:12:08 +0800

    init maven project,sso-client and sso-sever

---
+++ simple-sso-client/src/main/java/com/maslke/demo/LoginFilter.java
@@ -0,0 +1,42 @@
+package com.maslke.demo;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter {
+
+    private final String ssoUrl = "";
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("isLogin").equals("true")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        else {
+            response.sendRedirect(ssoUrl);
+        }
+    }
+}
